4f2d5302ced225e13251402a8042a08bdab6f1b2
kocsisakos <kocsis.akos@students.jedlik.eu>
2022-11-29 22:19:15 +0100

    breaking sidebar cuz braindead

--- a.js
+++ a.js
@@ -1,9 +1,20 @@
 /* Set the width of the side navigation to 250px */
+function openNavBtn() {
+    document.getElementById("sideNav").style.width = "20px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNavBtn() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
+/* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-} 
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
