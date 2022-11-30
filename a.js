3c80515a660873d474dfb7856951a695c10f22ec
kocsisakos <kocsis.akos@students.jedlik.eu>
2022-11-30 20:19:55 +0100

    dropBtn animalas

--- a.js
+++ a.js
@@ -1,18 +1,30 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function responsive() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+}
 }
 
 function openDrop() {
-    var y = document.getElementById("dropdown-content")
-    if (y.className === "hide") {
-        y.className += " show";
-    } else {
-        y.className = "hide";
-    }
+  var y = document.getElementById("dropdown-content")
+  var z = document.getElementById("dropbtnbtn")
+  if (y.className === "hide") {
+    y.className += " show";
+  } else {
+    y.className = "hide";
+  }
+  
+  if (z.className === "normal") {
+    z.className = "rotate";
+  } else {
+    z.className = "normal";
+  }
 }
+
+// function rotate() {
+  
+//   if (z.classname === "normal")
+// }
