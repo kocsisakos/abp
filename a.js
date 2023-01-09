7fa2cdbe3df3947bc0b141584272544c01f4cafb
Akos <kocsis.akos@students.jedlik.eu>
2023-01-09 19:39:30 +0100

    index 0.2

--- a.js
+++ a.js
@@ -68,15 +68,13 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("Slides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+  let small = document.getElementsByClassName("demo");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {slides[i].style.display = "none";}
-  for (i = 0; i < dots.length; i++) {dots[i].className = dots[i].className.replace(" active", "");}
+  for (i = 0; i < small.length; i++) {small[i].className = small[i].className.replace(" active", "");}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  small[slideIndex-1].className += " active";
 } 
 
 while (true) {
@@ -88,7 +86,3 @@ while (true) {
   }
 }
 
-function indexSlide(n) {
-  let slide = document.getElementsByClassName("indexmaincard")
-  
-}
